aa683e8cb12533fd576dc835cca6b72f071b594e
erikc <erikciqueira.silva@gmail.com>
2021-03-02 17:20:17 -0300

    implementação da segunda activity

--- app/src/main/java/presentation/characters/CharactersAdapter.kt
+++ app/src/main/java/presentation/characters/CharactersAdapter.kt
@@ -1,5 +1,7 @@
 package presentation.characters
 
+
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,21 +11,21 @@ import data.model.Character
 import kotlinx.android.synthetic.main.item_character.view.*
 
 class CharactersAdapter(
-      private  val characters: List<Character>
+    private val characters: List<Character>
 ) : RecyclerView.Adapter<CharactersAdapter.CharactersViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharactersViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_character, parent, false)
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_character, parent, false)
         return CharactersViewHolder(itemView)
-
     }
 
     override fun getItemCount() = characters.count()
 
     override fun onBindViewHolder(viewHolder: CharactersViewHolder, position: Int) {
         viewHolder.bindView(characters[position])
-
     }
+
     class CharactersViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         private val title = itemView.textTitle
@@ -33,7 +35,37 @@ class CharactersAdapter(
             title.text = character.title
             author.text = character.author
 
-
+            title.setOnClickListener {
+                val intent = Intent(it.context, CharacterDetailActivity::class.java)
+                it.context.startActivity(intent)
+            }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
